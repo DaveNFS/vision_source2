5984af3aca74ded8b8ef680e1e51e4c516d50791
adave <adave@adobe.com>
2015-04-05 17:34:34 -0600

    none

---
+++ two.php
@@ -0,0 +1,26 @@
+<html>
+
+<body>
+	<table>
+		<?php 
+
+
+		foreach ($_POST as $key => $value) 
+		{
+			echo "<tr>";
+			echo "<td>";
+			echo $key;
+			echo "</td>";
+			echo "<td>";
+			echo $value;
+			echo "</td>";
+			echo "</tr>";
+		}
+
+
+		?>
+	</table>
+
+</body>
+
+</html>
